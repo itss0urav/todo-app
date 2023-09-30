d8a14dcaa964596383ae2c7be5c0039748becece
itss0urav <sourav2021official@gmail.com>
2023-09-30 20:07:38 +0530

    updates

--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const todo = () => {
+const Todo = () => {
+  const [todo, setTodo] = useState([]);
+  const [inpTodo, setInpTodo] = useState("");
+  const [todoEdit, setTodoEdit] = useState(null);
+
+  const handleSubmit = () => {
+    setTodo([...todo, inpTodo]);
+  };
+
   return (
-    <div>todo</div>
-  )
-}
+    <div className="">
+      <div className="bg-black flex justify-center gap-2 p-3">
+        <input
+          value={inpTodo}
+          onChange={(e) => setInpTodo(e.target.value)}
+          placeholder="Enter your tasks"
+          className="rounded-md text-center"
+          type="text"
+        />
+        <button
+          onClick={handleSubmit}
+          className="hover:bg-gray-800 rounded-md p-1 text-white bg-gray-700"
+        >
+          Submit
+        </button>
+      </div>
+      <div className="text-center">
+        {todo.map((data, index) => (
+          <p key={index}>
+            {data}
+            <button className="bg-black m-1 hover:bg-gray-400 text-white rounded-md p-1">
+              Edit
+            </button>
+            <button className="bg-red-600 m-1 hover:bg-red-700 text-white rounded-md p-1">
+              Delete
+            </button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default todo
+export default Todo;
